720bd7e5a28c140cfd87a582d7904e9af828721e
Jack Portilla <jackportilla24@gmail.com>
2024-01-22 01:48:27 -0500

    agregue login

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 import init from 'react_native_mqtt';
+
 
 init({
   size: 10000,
@@ -13,7 +14,7 @@ init({
 });
 
 const options = {
-  host: '192.168.100.11', //PONEN EL IP DE SU PC
+  host: '192.168.1.2',
   port: 8083,
   path: '/mqtt',
   id: 'id_' + parseInt(Math.random() * 100000),
@@ -25,11 +26,18 @@ export default function App() {
   const [status, setStatus] = useState('');
   const [subscribedTopics, setSubscribedTopics] = useState(['temperatura', 'voltaje', 'intensidad', 'distancia']);
   const [topicData, setTopicData] = useState({});
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isConnected, setIsConnected] = useState(false); // Asegúrate de tener esta línea
+  const [error, setError] = useState('');
 
   useEffect(() => {
+    // Conectar cuando el componente se monta
     connect();
+
+    // Limpiar la conexión cuando el componente se desmonta
     return () => {
-      // Cleanup: disconnect when component unmounts
       client.disconnect();
     };
   }, []);
@@ -37,8 +45,15 @@ export default function App() {
   const onConnect = () => {
     console.log('onConnect');
     setStatus('connected');
-    // Subscribe to all specified topics
+    setLoggedIn(false);
+    setError('');
     subscribedTopics.forEach((topic) => subscribeTopic(topic));
+  };
+
+  const onConnectionLost = (responseObject) => {
+    console.log('Connection lost:', responseObject.errorMessage);
+    setStatus('disconnected');
+    setIsConnected(false);
   };
 
   const onFailure = (err) => {
@@ -54,8 +69,8 @@ export default function App() {
       useSSL: false,
       timeout: 3,
       onFailure: onFailure,
-      userName: 'prueba',
-      password: '1234',
+      userName: 'Cliente',
+      password: '12345',
       cleanSession: true,
     });
   };
@@ -77,20 +92,72 @@ export default function App() {
     }));
   };
 
-  // Set up message arrived callback
+
   client.onMessageArrived = onMessageArrived;
+
+  const handleLogin = async () => {
+    try {
+      if (username === 'Admin' && password === '12345') {
+        setLoggedIn(true);
+        setError('');
+      } else {
+        setLoggedIn(false);
+        setError('Usuario no registrado');
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+      setError('Error al iniciar sesión');
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setUsername('');
+    setPassword('');
+  };
+
 
   return (
     <View style={styles.container}>
-      <Text>Status: {status}</Text>
-      <Text>Topic Data:</Text>
-      {subscribedTopics.map((topic) => (
-        <Text key={topic}>
-          {topic}: {topicData[topic] || 'No data'}
-        </Text>
-      ))}
-      <StatusBar style="auto" />
-    </View>
+      
+    {isLoggedIn ? (
+      <View style={styles.loggedInContainer}>
+        <Text style={styles.loggedInTitle}>Bienvenido</Text>
+        <Text>Status: {status}</Text>
+        <Text>Topic Data:</Text>
+        {subscribedTopics.map((topic) => (
+          <Text key={topic}>
+            {topic}: {topicData[topic] || 'No data'}
+          </Text>
+        ))}
+        <Button title="Salir" onPress={handleLogout} style={styles.salirButton} />
+        <StatusBar style="auto" />
+      </View>
+    ) : (
+      <View style={styles.loginContainer}>
+        <Image
+      source={require('./assets/img/imagen5.png')}
+      style={styles.image}
+    />
+        <Text style={styles.loginTitle}>Inicio de Sesión</Text>
+        <TextInput
+          placeholder="Nombre de Usuario"
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Contraseña"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+          style={styles.input}
+        />
+        <Button title="Iniciar Sesión" onPress={handleLogin} style={styles.loginButton} />
+        {error ? <Text style={styles.errorText}>{error}</Text> : null}
+      </View>
+    )}
+  </View>
   );
 }
 
@@ -100,5 +167,51 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  loggedInContainer: {
+    alignItems: 'center',
+  },
+  loggedInTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  salirButton: {
+    backgroundColor: 'red',
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  loginContainer: {
+    width: '80%',
+    alignItems: 'center',
+  },
+  loginTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    width: '100%',
+  },
+  loginButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 10,
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain', // Puedes ajustar esto según tus necesidades
   },
 });
